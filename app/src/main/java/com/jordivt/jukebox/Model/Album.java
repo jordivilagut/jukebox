7ab1142c14f0a41a474a41cbecabb936d609e56e
jordivilagut <vilagutjordi@gmail.com>
2017-11-17 17:42:47 +0100

    Map Album object

--- app/src/main/java/com/jordivt/jukebox/Model/Album.java
+++ app/src/main/java/com/jordivt/jukebox/Model/Album.java
@@ -2,9 +2,40 @@ package com.jordivt.jukebox.Model;
 
 public class Album {
 
-    private String wrapperType;
-    private String collectionType;
-    private long artistId;
     private String artistName;
     private String collectionName;
+    private String artworkUrl60;
+    private int trackCount;
+
+    public String getArtistName() {
+        return artistName;
+    }
+
+    public void setArtistName(String artistName) {
+        this.artistName = artistName;
+    }
+
+    public String getCollectionName() {
+        return collectionName;
+    }
+
+    public void setCollectionName(String collectionName) {
+        this.collectionName = collectionName;
+    }
+
+    public String getArtworkUrl60() {
+        return artworkUrl60;
+    }
+
+    public void setArtworkUrl60(String artworkUrl60) {
+        this.artworkUrl60 = artworkUrl60;
+    }
+
+    public int getTrackCount() {
+        return trackCount;
+    }
+
+    public void setTrackCount(int trackCount) {
+        this.trackCount = trackCount;
+    }
 }
